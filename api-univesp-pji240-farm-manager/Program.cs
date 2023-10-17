649f2f2a61c6080ad8bd2d63e6a997718c7faa38
IanCicoloMarques <50027597+IanCicoloMarques@users.noreply.github.com>
2023-10-17 19:23:10 -0300

    Remove https redirection

--- api-univesp-pji240-farm-manager/Program.cs
+++ api-univesp-pji240-farm-manager/Program.cs
@@ -28,13 +28,13 @@ builder.Services.AddSingleton<IDataOrderRepository, DataOrderRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
